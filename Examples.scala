a965d27f75e1d95e57f6f90dc7ffe810724f3ef7
Wert_Lex <a.v.povar@gmail.com>
2015-05-24 13:48:28 +0600

    initial sbt layout

---
+++ Examples.scala
@@ -0,0 +1,8 @@
+package com.github.wertlex
+
+object Hello {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ifnh"
+
+version := "0.1"
+
+scalaVersion := "2.11.6"
